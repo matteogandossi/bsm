9d3e56cd1dd8a194c37f2276b7bbf07a7d92cafb
matteogandossi <matteo.gandossi@studenti.unimi.it>
2019-05-20 11:02:29 +0200

    added a missing attribute

--- src/bsm/User.java
+++ src/bsm/User.java
@@ -13,6 +13,7 @@ public class User {
     private String logIn;
     private String emailId;
     private String password;
+    private String designation;
     private String department;
     private String mobileNumber;
     private String dateOfBirth;
@@ -23,6 +24,8 @@ public class User {
     
     public void setFirstName(String firstName) { this.firstName = firstName; }
     public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
     public String getName() { return firstName + " " +  lastName; }
     
     public void setId(String id) { this.id = id; }
@@ -42,6 +45,9 @@ public class User {
     
     public void setDepartment(String department) { this.department = department; }
     public String getDepartment() { return department; }
+    
+    public void setDesignation(String designation) { this.designation = designation; }
+    public String getDesignation() { return designation; }
     
     public void setMobileNumber(String mobileNumber) { this.mobileNumber = mobileNumber; }
     public String getMobileNumber() { return mobileNumber; }
